d47453f87c264f9cc602c46da308db9dc2d81e3d
hushline-dev <git-dev@scidsg.org>
2026-06-10 12:24:28 -0700

    Rebuild library docs

---
+++ src/library/assets/js/787315af.d6ac3885.js
@@ -0,0 +1,192 @@
+"use strict";
+(globalThis["webpackChunkdocs"] = globalThis["webpackChunkdocs"] || []).push([[859],{
+
+/***/ 1015
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/cover-45d39fa572b924dfb0d5ac8ba038a159.png");
+
+/***/ },
+
+/***/ 1535
+(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/library/blog/why-a-signal-tip-line-isnt-enough","editUrl":"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/blog/2025-02-16-why-a-signal-tip-line-isnt-enough/index.md","source":"@site/blog/2025-02-16-why-a-signal-tip-line-isnt-enough/index.md","title":"Why A Signal Tip Line Isn\'t Enough","description":"Encryption settings wireframe","date":"2025-02-16T00:00:00.000Z","tags":[{"inline":false,"label":"Hush Line","permalink":"/library/blog/tags/hushline","description":"Hush Line articles"},{"inline":false,"label":"Signal","permalink":"/library/blog/tags/signal","description":"Signal articles"}],"readingTime":3.97,"hasTruncateMarker":true,"authors":[{"name":"Glenn Sorrentino","title":"Executive Director, Science & Design","url":"https://scidsg.org","page":{"permalink":"/library/blog/authors/gsorrentino"},"socials":{"github":"https://github.com/glenn-sorrentino"},"imageURL":"/library/img/glenn.jpg","key":"gsorrentino"}],"frontMatter":{"slug":"why-a-signal-tip-line-isnt-enough","title":"Why A Signal Tip Line Isn\'t Enough","subtitle":"Why Signal as a catch-all isn’t enough, and a data-informed way to think about your solution set","authors":["gsorrentino"],"tags":["hushline","signal"],"image":"img/cover.png"},"unlisted":false,"prevItem":{"title":"How to Verify an Address on Hush Line","permalink":"/library/blog/how-to-verify-an-address-on-hush-line"}}');
+
+/***/ },
+
+/***/ 7062
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   assets: () => (/* binding */ assets),
+/* harmony export */   contentTitle: () => (/* binding */ contentTitle),
+/* harmony export */   "default": () => (/* binding */ MDXContent),
+/* harmony export */   frontMatter: () => (/* binding */ frontMatter),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_2025_02_16_why_a_signal_tip_line_isnt_enough_index_md_332_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   toc: () => (/* binding */ toc)
+/* harmony export */ });
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_default_site_blog_2025_02_16_why_a_signal_tip_line_isnt_enough_index_md_332_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1535);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(4848);
+/* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(8453);
+
+
+const frontMatter = {
+	slug: 'why-a-signal-tip-line-isnt-enough',
+	title: 'Why A Signal Tip Line Isn\'t Enough',
+	subtitle: 'Why Signal as a catch-all isn’t enough, and a data-informed way to think about your solution set',
+	authors: [
+		'gsorrentino'
+	],
+	tags: [
+		'hushline',
+		'signal'
+	],
+	image: 'img/cover.png'
+};
+const contentTitle = undefined;
+
+const assets = {
+"authorsImageUrls": [undefined],
+};
+
+
+
+const toc = [];
+function _createMdxContent(props) {
+  const _components = {
+    img: "img",
+    p: "p",
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.Fragment, {
+    children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
+      children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.img, {
+        alt: "Encryption settings wireframe",
+        src: (__webpack_require__(1015)/* ["default"] */ .A) + "",
+        width: "3834",
+        height: "1880"
+      })
+    }), "\n", (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
+      children: "I love Signal. I was almost their first full-time designer when the team was only four people with a physical office back in the Mission in San Francisco. I turned the offer down because I was too junior in my career to feel like I could be as effective as I knew I could be with more time at the Big Tech company I worked for..."
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponents */ .R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(MDXLayout, {
+    ...props,
+    children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ },
+
+/***/ 8453
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   R: () => (/* binding */ useMDXComponents),
+/* harmony export */   x: () => (/* binding */ MDXProvider)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6540);
+/**
+ * @import {MDXComponents} from 'mdx/types.js'
+ * @import {Component, ReactElement, ReactNode} from 'react'
+ */
+
+/**
+ * @callback MergeComponents
+ *   Custom merge function.
+ * @param {Readonly<MDXComponents>} currentComponents
+ *   Current components from the context.
+ * @returns {MDXComponents}
+ *   Additional components.
+ *
+ * @typedef Props
+ *   Configuration for `MDXProvider`.
+ * @property {ReactNode | null | undefined} [children]
+ *   Children (optional).
+ * @property {Readonly<MDXComponents> | MergeComponents | null | undefined} [components]
+ *   Additional components to use or a function that creates them (optional).
+ * @property {boolean | null | undefined} [disableParentContext=false]
+ *   Turn off outer component context (default: `false`).
+ */
+
+
+
+/** @type {Readonly<MDXComponents>} */
+const emptyComponents = {}
+
+const MDXContext = react__WEBPACK_IMPORTED_MODULE_0__.createContext(emptyComponents)
+
+/**
+ * Get current components from the MDX Context.
+ *
+ * @param {Readonly<MDXComponents> | MergeComponents | null | undefined} [components]
+ *   Additional components to use or a function that creates them (optional).
+ * @returns {MDXComponents}
+ *   Current components.
+ */
+function useMDXComponents(components) {
+  const contextComponents = react__WEBPACK_IMPORTED_MODULE_0__.useContext(MDXContext)
+
+  // Memoize to avoid unnecessary top-level context changes
+  return react__WEBPACK_IMPORTED_MODULE_0__.useMemo(
+    function () {
+      // Custom merge via a function prop
+      if (typeof components === 'function') {
+        return components(contextComponents)
+      }
+
+      return {...contextComponents, ...components}
+    },
+    [contextComponents, components]
+  )
+}
+
+/**
+ * Provider for MDX context.
+ *
+ * @param {Readonly<Props>} properties
+ *   Properties.
+ * @returns {ReactElement}
+ *   Element.
+ * @satisfies {Component}
+ */
+function MDXProvider(properties) {
+  /** @type {Readonly<MDXComponents>} */
+  let allComponents
+
+  if (properties.disableParentContext) {
+    allComponents =
+      typeof properties.components === 'function'
+        ? properties.components(emptyComponents)
+        : properties.components || emptyComponents
+  } else {
+    allComponents = useMDXComponents(properties.components)
+  }
+
+  return react__WEBPACK_IMPORTED_MODULE_0__.createElement(
+    MDXContext.Provider,
+    {value: allComponents},
+    properties.children
+  )
+}
+
+
+/***/ }
+
+}]);
---
+++ src/library/assets/js/912d1017.cd5ec21a.js
@@ -0,0 +1,315 @@
+"use strict";
+(globalThis["webpackChunkdocs"] = globalThis["webpackChunkdocs"] || []).push([[3404],{
+
+/***/ 5082
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/auth-newman-onboarding-profile-desktop-light-fold-0be291280102626be19c7855340c35fd.png");
+
+/***/ },
+
+/***/ 5990
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/auth-newman-onboarding-encryption-desktop-light-fold-527c725b0a3db577783c5ff9ef9292a6.png");
+
+/***/ },
+
+/***/ 6599
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/auth-newman-inbox-desktop-light-fold-52959f738fc315196571d27fc85b27d6.png");
+
+/***/ },
+
+/***/ 6762
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  assets: () => (/* binding */ assets),
+  contentTitle: () => (/* binding */ contentTitle),
+  "default": () => (/* binding */ MDXContent),
+  frontMatter: () => (/* binding */ frontMatter),
+  metadata: () => (/* reexport */ site_docs_getting_started_new_user_onboarding_md_912_namespaceObject),
+  toc: () => (/* binding */ toc)
+});
+
+;// ./.docusaurus/docusaurus-plugin-content-docs/default/site-docs-getting-started-new-user-onboarding-md-912.json
+const site_docs_getting_started_new_user_onboarding_md_912_namespaceObject = /*#__PURE__*/JSON.parse('{"id":"getting-started/new-user-onboarding","title":"New User Onboarding","description":"New user onboarding is a guided, four-step process that will result in a fully functional account that\'s ready to receive secure, anonymous tips.","source":"@site/docs/getting-started/new-user-onboarding.md","sourceDirName":"getting-started","slug":"/getting-started/new-user-onboarding","permalink":"/library/docs/getting-started/new-user-onboarding","draft":false,"unlisted":false,"editUrl":"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/docs/getting-started/new-user-onboarding.md","tags":[],"version":"current","sidebarPosition":2,"frontMatter":{"sidebar_position":2,"id":"new-user-onboarding","title":"New User Onboarding"},"sidebar":"tutorialSidebar","previous":{"title":"Start Here","permalink":"/library/docs/getting-started/start-here"},"next":{"title":"Prep Your Account","permalink":"/library/docs/getting-started/account-setup"}}');
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(4848);
+// EXTERNAL MODULE: ./node_modules/@mdx-js/react/lib/index.js
+var lib = __webpack_require__(8453);
+;// ./docs/getting-started/new-user-onboarding.md
+
+
+const frontMatter = {
+	sidebar_position: 2,
+	id: 'new-user-onboarding',
+	title: 'New User Onboarding'
+};
+const contentTitle = 'New User Onboarding';
+
+const assets = {
+
+};
+
+
+
+const toc = [{
+  "value": "Step 1. Add Information About Yourself",
+  "id": "step-1-add-information-about-yourself",
+  "level": 2
+}, {
+  "value": "Step 2. Enable End-to-End Encryption",
+  "id": "step-2-enable-end-to-end-encryption",
+  "level": 2
+}, {
+  "value": "Step 3. Enable Message Forwarding",
+  "id": "step-3-enable-message-forwarding",
+  "level": 2
+}, {
+  "value": "Step 4. Add Yourself to the User Directory",
+  "id": "step-4-add-yourself-to-the-user-directory",
+  "level": 2
+}, {
+  "value": "Skipping Onboarding",
+  "id": "skipping-onboarding",
+  "level": 2
+}];
+function _createMdxContent(props) {
+  const _components = {
+    br: "br",
+    h1: "h1",
+    h2: "h2",
+    header: "header",
+    hr: "hr",
+    img: "img",
+    p: "p",
+    strong: "strong",
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return (0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+    children: [(0,jsx_runtime.jsx)(_components.header, {
+      children: (0,jsx_runtime.jsx)(_components.h1, {
+        id: "new-user-onboarding",
+        children: "New User Onboarding"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "New user onboarding is a guided, four-step process that will result in a fully functional account that's ready to receive secure, anonymous tips."
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "step-1-add-information-about-yourself",
+      children: "Step 1. Add Information About Yourself"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Add a clear bio, extra links, or your Signal username to help your community."
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: (0,jsx_runtime.jsx)(_components.img, {
+        alt: "Settings",
+        src: (__webpack_require__(5082)/* ["default"] */ .A) + "",
+        width: "1000",
+        height: "800"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "step-2-enable-end-to-end-encryption",
+      children: "Step 2. Enable End-to-End Encryption"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Add an encryption key so you can receive messages securely:"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: (0,jsx_runtime.jsx)(_components.img, {
+        alt: "Encryption settings wireframe",
+        src: (__webpack_require__(5990)/* ["default"] */ .A) + "",
+        width: "1000",
+        height: "800"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "step-3-enable-message-forwarding",
+      children: "Step 3. Enable Message Forwarding"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Forward messages so you don’t have to log back in. If you imported a key from Proton in the previous step, you should use the same email address here."
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: (0,jsx_runtime.jsx)(_components.img, {
+        alt: "Email",
+        src: (__webpack_require__(8191)/* ["default"] */ .A) + "",
+        width: "1000",
+        height: "800"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "step-4-add-yourself-to-the-user-directory",
+      children: "Step 4. Add Yourself to the User Directory"
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: "Opt-in to the public user directory so whistleblowers can find your account."
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: (0,jsx_runtime.jsx)(_components.img, {
+        alt: "Directory",
+        src: (__webpack_require__(9188)/* ["default"] */ .A) + "",
+        width: "1000",
+        height: "800"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.h2, {
+      id: "skipping-onboarding",
+      children: "Skipping Onboarding"
+    }), "\n", (0,jsx_runtime.jsxs)(_components.p, {
+      children: ["If you skip onboarding, you can easily access it again using the ", (0,jsx_runtime.jsx)(_components.strong, {
+        children: "Account Setup"
+      }), " link in the header."]
+    }), "\n", (0,jsx_runtime.jsx)(_components.p, {
+      children: (0,jsx_runtime.jsx)(_components.img, {
+        alt: "Empty Inbox",
+        src: (__webpack_require__(6599)/* ["default"] */ .A) + "",
+        width: "1000",
+        height: "800"
+      })
+    }), "\n", (0,jsx_runtime.jsx)(_components.hr, {}), "\n", (0,jsx_runtime.jsxs)(_components.p, {
+      children: ["🎉 ", (0,jsx_runtime.jsx)(_components.strong, {
+        children: "Congratulations!"
+      }), (0,jsx_runtime.jsx)(_components.br, {}), "\n", "Now that you're set up, share your tip line with your organization or community. Next, we'll secure your Hush Line account."]
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = {
+    ...(0,lib/* useMDXComponents */.R)(),
+    ...props.components
+  };
+  return MDXLayout ? (0,jsx_runtime.jsx)(MDXLayout, {
+    ...props,
+    children: (0,jsx_runtime.jsx)(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+
+
+/***/ },
+
+/***/ 8191
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/auth-newman-onboarding-notifications-desktop-light-fold-2d07f211be06ac7aa38c8ef16efdce46.png");
+
+/***/ },
+
+/***/ 8453
+(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   R: () => (/* binding */ useMDXComponents),
+/* harmony export */   x: () => (/* binding */ MDXProvider)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6540);
+/**
+ * @import {MDXComponents} from 'mdx/types.js'
+ * @import {Component, ReactElement, ReactNode} from 'react'
+ */
+
+/**
+ * @callback MergeComponents
+ *   Custom merge function.
+ * @param {Readonly<MDXComponents>} currentComponents
+ *   Current components from the context.
+ * @returns {MDXComponents}
+ *   Additional components.
+ *
+ * @typedef Props
+ *   Configuration for `MDXProvider`.
+ * @property {ReactNode | null | undefined} [children]
+ *   Children (optional).
+ * @property {Readonly<MDXComponents> | MergeComponents | null | undefined} [components]
+ *   Additional components to use or a function that creates them (optional).
+ * @property {boolean | null | undefined} [disableParentContext=false]
+ *   Turn off outer component context (default: `false`).
+ */
+
+
+
+/** @type {Readonly<MDXComponents>} */
+const emptyComponents = {}
+
+const MDXContext = react__WEBPACK_IMPORTED_MODULE_0__.createContext(emptyComponents)
+
+/**
+ * Get current components from the MDX Context.
+ *
+ * @param {Readonly<MDXComponents> | MergeComponents | null | undefined} [components]
+ *   Additional components to use or a function that creates them (optional).
+ * @returns {MDXComponents}
+ *   Current components.
+ */
+function useMDXComponents(components) {
+  const contextComponents = react__WEBPACK_IMPORTED_MODULE_0__.useContext(MDXContext)
+
+  // Memoize to avoid unnecessary top-level context changes
+  return react__WEBPACK_IMPORTED_MODULE_0__.useMemo(
+    function () {
+      // Custom merge via a function prop
+      if (typeof components === 'function') {
+        return components(contextComponents)
+      }
+
+      return {...contextComponents, ...components}
+    },
+    [contextComponents, components]
+  )
+}
+
+/**
+ * Provider for MDX context.
+ *
+ * @param {Readonly<Props>} properties
+ *   Properties.
+ * @returns {ReactElement}
+ *   Element.
+ * @satisfies {Component}
+ */
+function MDXProvider(properties) {
+  /** @type {Readonly<MDXComponents>} */
+  let allComponents
+
+  if (properties.disableParentContext) {
+    allComponents =
+      typeof properties.components === 'function'
+        ? properties.components(emptyComponents)
+        : properties.components || emptyComponents
+  } else {
+    allComponents = useMDXComponents(properties.components)
+  }
+
+  return react__WEBPACK_IMPORTED_MODULE_0__.createElement(
+    MDXContext.Provider,
+    {value: allComponents},
+    properties.children
+  )
+}
+
+
+/***/ },
+
+/***/ 9188
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/auth-newman-onboarding-directory-desktop-light-fold-7cca4b8a055bc44d7acfe68dad51c090.png");
+
+/***/ }
+
+}]);
